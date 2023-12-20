5716f10e894690941601cef3f45f045feec6eeb7
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-20 16:27:53 +0600

    add tailwind and deisyUI

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import MainPage from './componetns/MainPage'
 
 const router=createBrowserRouter([
   {
     path:'/',
-    element:<div>hello world</div>
+    element:<MainPage></MainPage>
   }
 ])
 
